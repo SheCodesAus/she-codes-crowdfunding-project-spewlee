341d5bb65eb930bf1abe68319c966e8f78677c15
spewlee <suelee_1@hotmail.com>
2022-05-14 00:45:48 +0800

    More adjustments to the Register Form

--- src/components/HeroImage/HeroImage.jsx
+++ src/components/HeroImage/HeroImage.jsx
@@ -4,11 +4,14 @@ import { useNavigate } from "react-router-dom";
 //CSS
 import "./HeroImage.css";
 
-// Hooks
-const navigate = useNavigate();
-const handleClick = navigate("/create-project")
+
 
 function HeroImage() {
+
+    // Hooks
+    const navigate = useNavigate();
+    const handleClick = navigate("/create-project") 
+    
     return(
         <div className="heroImage">
           <button onclick={ handleClick }>Post a Project</button>
